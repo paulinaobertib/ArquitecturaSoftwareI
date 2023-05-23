0b92e8b54a8f973eb1a2402d9b89e522f0da42c1
VickyVaccarini <vicky_vaccarini@hotmail.com>
2023-05-23 20:20:49 -0300

    Controller amenitie and hotel

---
+++ controllers/amenitie/amenitie_controller.go
@@ -0,0 +1,59 @@
+package amenitieController
+
+import (
+	"booking-api/dto"
+	service "booking-api/service"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+func GetAmenitieById(c *gin.Context) {
+	log.Debug("Amenitie id to load: " + c.Param("id"))
+
+	id, _ := strconv.Atoi(c.Param("id")) // es id entonces vale
+	var amenitieDto dto.AmenitieDto
+
+	amenitieDto, err := service.AmenitieService.GetAmenitieById(id)
+
+	if err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+	c.JSON(http.StatusOK, amenitieDto) // devuelve el estado 200 como respuesta si todo salio bien
+}
+
+func GetAmenities(c *gin.Context) {
+	var amenitiesDto dto.AmenitiesDto
+	amenitiesDto, err := service.AmenitieService.GetAmenities()
+
+	if err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+
+	c.JSON(http.StatusOK, amenitiesDto)
+}
+
+func AmenitieInsert(c *gin.Context) {
+	var amenitieDto dto.AmenitieDto
+	err := c.BindJSON(&amenitieDto)
+
+	// Error Parsing json param
+	if err != nil {
+		log.Error(err.Error())
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	amenitieDto, er := service.AmenitieService.InsertAmenitie(amenitieDto) // llama a la funcion del service
+	// Error del Insert
+	if er != nil {
+		c.JSON(er.Status(), er)
+		return
+	}
+
+	c.JSON(http.StatusCreated, amenitieDto) // estos son los mensajes que se muestran, en este caso seria el creado, 201
+}
---
+++ controllers/hotel/hotel_controller.go
@@ -0,0 +1,59 @@
+package hotelController
+
+import (
+	"booking-api/dto"
+	service "booking-api/service"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+func GetHotelById(c *gin.Context) {
+	log.Debug("Hotel id to load: " + c.Param("id"))
+
+	id, _ := strconv.Atoi(c.Param("id")) // es id entonces vale
+	var hotelDto dto.HotelDto
+
+	hotelDto, err := service.HotelService.GetHotelById(id)
+
+	if err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+	c.JSON(http.StatusOK, hotelDto) // devuelve el estado 200 como respuesta si todo salio bien
+}
+
+func GetHotels(c *gin.Context) {
+	var hotelsDto dto.HotelsDto
+	hotelsDto, err := service.HotelService.GetHotels()
+
+	if err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+
+	c.JSON(http.StatusOK, hotelsDto)
+}
+
+func HotelInsert(c *gin.Context) {
+	var hotelDto dto.HotelDto
+	err := c.BindJSON(&hotelDto)
+
+	// Error Parsing json param
+	if err != nil {
+		log.Error(err.Error())
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	hotelDto, er := service.HotelService.InsertHotel(hotelDto) // llama a la funcion del service
+	// Error del Insert
+	if er != nil {
+		c.JSON(er.Status(), er)
+		return
+	}
+
+	c.JSON(http.StatusCreated, hotelDto) // estos son los mensajes que se muestran, en este caso seria el creado, 201
+}
