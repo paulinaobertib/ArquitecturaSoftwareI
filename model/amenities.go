e8156c4c2341050dc238d2c1c1a2ae271a694262
paulinaobertib <paulinaobertibusso@gmail.com>
2023-05-23 14:24:11 -0300

    amenitie

--- model/amenities.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Amenitie struct {
-	Id          int    `gorm:"primaryKey"`
-	Name        string `gorm:"type:varchar(350);not null"`
-	Description string `gorm:"type:varchar(500);not null"`
-}
-
-type Amenities []Amenitie
