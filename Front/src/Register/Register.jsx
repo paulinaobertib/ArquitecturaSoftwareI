72d395cf22ba36af1a2be7ddd8e264ae95caee8a
Simon <hernandezsimon84@gmail.com>
2023-06-02 14:01:26 -0300

    completar register

--- Front/src/Register/Register.jsx
+++ Front/src/Register/Register.jsx
@@ -56,6 +56,9 @@ export function Register() {
         showClass: {
           popup: "animate__animated animate__fadeInDown",
         },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        }
       });
     }
   };
