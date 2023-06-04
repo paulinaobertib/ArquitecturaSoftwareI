8e6c0fee9c2287d44c4acb78ca58722d35bf29f2
paulinaobertib <paulinaobertibusso@gmail.com>
2023-06-03 21:01:39 -0300

    anda date picker

--- Front/src/Components/DatePicker.jsx
+++ Front/src/Components/DatePicker.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import "./../Home/home.css";
 import { BASE_URL } from "../configs";
+import Card from "./Card";
 
 const DatePicker = () => {
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
   const [hotels, setHotels] = useState([]);
+  const [hotelsShow, setHotelsShow] = useState([]);
   const [hotelAvailability, setHotelAvailability] = useState({});
 
   const handleSubmit = async () => {
@@ -18,19 +20,33 @@ const DatePicker = () => {
       for (const key of keys) {
         const hotel = hotelsData[key];
         for (const hotelA of hotel) {
-          console.log("ACAA", hotel);
+          //console.log("ACAA", hotel);
+          //console.log("hotela", hotelA);
           const hotelID = hotelA.id;
-          console.log("ACA ID", hotelID);
+          //console.log("ACA ID", hotelID);
 
           const availabilityResponse = await fetch(`${BASE_URL}/booking/availability/${hotelID}/${startDate}/${endDate}`);
           const availabilityData = await availabilityResponse.json();
 
+          if (availabilityData.rooms_available > 0) {
+            setHotelsShow(prevHotels => [...prevHotels, hotelA]);
+            //console.log("si entro");
+          } else {
+            console.log("no hay");
+          }
+          
           setHotelAvailability(prevAvailability => ({
             ...prevAvailability,
             [hotelID]: availabilityData
           }));
+
+          //console.log("MIRA SI SIRVE", availabilityData);
         }
+
       }
+
+      console.log("MIRA MIRA", hotelsShow);
+      //console.log("ACA VER IMP",Object.keys(hotelAvailability).length);
 
     } catch (error) {
       console.error('Error al obtener los hoteles o la disponibilidad:', error);
@@ -66,6 +82,10 @@ const DatePicker = () => {
     };
   }, []);
 
+  //console.log("AHORA", hotelsShow.length);
+  //console.log("ACA VER",hotelAvailability.length);
+  //console.log("ACA MIRA", Object.values(hotelAvailability));
+
   return (
     <div className='dateDiv'>
       <section className='datePicker'>
@@ -82,8 +102,14 @@ const DatePicker = () => {
       <div className='SeccionHoteles'>
         <h2>Hoteles en las fechas seleccionadas</h2>
         <div className='HotelCard'>
-        
-      </div>
+        {hotelsShow.length ? (
+          hotelsShow.map((hotel) => (
+            <Card key={hotel.id} name={hotel.name} image={hotel.image} id={hotel.id} />
+          ))
+        ) : (
+          <p>No hay hoteles disponibles</p>
+        )}
+        </div>
       </div>
     </div>
   );
