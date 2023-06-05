4de4bccc21e57497a4e43631f6f2e10897cb8c8b
VickyVaccarini <vicky_vaccarini@hotmail.com>
2023-06-05 00:41:59 -0300

    perfil

--- Front/src/Perfil/Perfil.jsx
+++ Front/src/Perfil/Perfil.jsx
@@ -1,11 +1,12 @@
-import { useEffect, useContext, useState } from "react";
-
+import React, { useEffect, useContext, useState } from "react";
 import { AuthContext } from "../Providers/AuthContextProvider";
+import { BASE_URL } from "../configs";
 
 const ProfilePage = () => {
   const { user, getUserBookings } = useContext(AuthContext);
 
   const [userBookings, setUserBookings] = useState([]);
+  const [hotelNames, setHotelNames] = useState({});
 
   useEffect(() => {
     const fetchUserBookings = async () => {
@@ -14,6 +15,7 @@ const ProfilePage = () => {
           const bookingData = await getUserBookings(user.id);
           console.log("User Bookings:", bookingData);
           setUserBookings(bookingData);
+          fetchHotelNames(bookingData.bookings);
         }
       } catch (error) {
         console.error(error);
@@ -23,22 +25,38 @@ const ProfilePage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user, getUserBookings]);
 
-  if (!user) {
-    return <p>Cargando detalles del usuario...</p>;
-  }
+  const fetchHotelNames = async (bookings) => {
+    const hotelIds = bookings.map((booking) => booking.hotel_id);
+    const uniqueHotelIds = [...new Set(hotelIds)]; // Eliminar duplicados
+
+    try {
+      const hotelNamesMap = {};
+
+      for (const hotelId of uniqueHotelIds) {
+        const response = await fetch(`${BASE_URL}/hotel/${hotelId}`);
+        const data = await response.json();
+        hotelNamesMap[hotelId] = data.name;
+      }
+
+      setHotelNames(hotelNamesMap);
+    } catch (error) {
+      console.error("Error al obtener los nombres de los hoteles:", error);
+    }
+  };
+
   return (
     <div>
       <h2>Perfil de Usuario</h2>
-      <p>Nombre: {user.name}</p>
-      <p>Apellido: {user.last_name}</p>
-      <p>Email: {user.email}</p>
+      <p>Nombre: {user?.name}</p>
+      <p>Apellido: {user?.last_name}</p>
+      <p>Email: {user?.email}</p>
       <h3>Reservas:</h3>
       {userBookings && (
         <ul>
           {userBookings?.bookings?.map((booking) => (
             <li key={booking.id}>
               <p>ID de reserva: {booking.id}</p>
-              <p>Hotel: {booking.hotel_id}</p>
+              <p>Hotel: {hotelNames[booking.hotel_id]}</p>
               <p>Fecha de ingreso: {booking.date_from}</p>
               <p>Fecha de salida: {booking.date_to}</p>
               <p>------------------</p>
