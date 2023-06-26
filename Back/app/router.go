38326a429d82310b88e9923dabb62d9e0ad8a8b8
paulinaobertib <paulinaobertibusso@gmail.com>
2023-06-25 23:53:15 -0300

    fin

--- Back/app/router.go
+++ Back/app/router.go
@@ -1,7 +1,7 @@
 package app
 
 import (
-	"github.com/gin-contrib/cors"
+	cors "github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
@@ -12,8 +12,9 @@ var (
 
 func init() {
 	router = gin.Default()
+
 	router.Use(cors.Default())
-	// Ruta para servir archivos estáticos desde "imagenesHoteles"
+
 	router.Static("/imagenesHoteles", "./ImagenesHoteles")
 }
 
@@ -21,5 +22,7 @@ func StartRoute() {
 	mapUrls()
 
 	log.Info("Starting server")
+
 	router.Run(":8090")
+
 }
--- Front/src/configs.js
+++ Front/src/configs.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://backend:8090";
+//export const BASE_URL = "http://backend:8090";
 //export const BASE_URL = "http://localhost:8090";
-
+export const BASE_URL = "http://192.168.100.14:8090";
